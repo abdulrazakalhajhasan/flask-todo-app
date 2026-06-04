d851924069e697580717e2be7ca9efcc2dfe50ab
Abdul Razak Alhaj Hasan <abdulrazakalhajhasan@gmail.com>
2026-06-04 17:00:14 +0400

    fixed db error

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
-    
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
@@ -20,6 +18,9 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r>' % self.id
+    
+with app.app_context():
+    db.create_all()    
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
