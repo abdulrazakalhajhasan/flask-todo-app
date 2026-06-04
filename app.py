47ec76b0ba91fa9b7b90d287e43aaed2acc8b118
Abdul Razak Alhaj Hasan <abdulrazakalhajhasan@gmail.com>
2026-06-04 09:37:44 +0400

    auto create db

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
